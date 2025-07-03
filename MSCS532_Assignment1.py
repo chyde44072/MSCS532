b007f946789a6ea040013b20d4a49be5ddaed930
chyde44072 <chyde44072@ucumberlands.edu>
2025-07-02 22:11:13 -0400

    2

--- MSCS532_Assignment1.py
+++ MSCS532_Assignment1.py
@@ -1,2 +1,14 @@
 def insertion_sort_desc(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+# Example usage
+arr = [8, 3, 5, 2, 9]
+insertion_sort_desc(arr)
+print("Sorted (descending):", arr)
 
